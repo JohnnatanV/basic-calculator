ee9375da0e000059c21a9287220039ccf425af29
JohnnatanV <joh.villaneda@outlook.com>
2025-05-15 18:02:22 -0500

    Screen text logic complete, pending operations logic

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 const screen = document.getElementById("screen")
 const keys = document.querySelectorAll(".key")
+const operators = document.querySelectorAll(".operator")
 
-console.log(keys)
+let screenText = []
+
+const writeToScreen = (arr) => {
+  screen.innerText = arr.join("")
+  console.log(screen.innerText)
+}
+
+const addition = (a, b) => a + b
+const subtraction = (a, b) => a - b
+const multiplication = (a, b) => a * b
+const division = (a, b) => a / b
+const erase = () => screen.innerText = ""
+
 
 keys.forEach((key) => {
   key.addEventListener("click", (e) => {
+    e.preventDefault()
     let keyPress = e.target.innerText
-    console.log(keyPress)
+    screenText.push(keyPress)
+    writeToScreen(screenText)
   })
 })
 
-const writeToScreen = (value) => {
-  let currentValue = screen.innerText
-}
 
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    e.preventDefault()
+    let keyOperator = e.target.innerText
+  })
+})
