edfa778403c2e7d08c7c279aa1b3964a57bad644
JohnnatanV <joh.villaneda@outlook.com>
2025-05-16 23:50:39 -0500

    Calculation logic for operations, pending for corrections, check handling operators

--- src/app.js
+++ src/app.js
@@ -6,14 +6,16 @@ let screenText = []
 
 const writeToScreen = (arr) => {
   screen.innerText = arr.join("")
-  console.log(screen.innerText)
 }
 
 const addition = (a, b) => a + b
 const subtraction = (a, b) => a - b
 const multiplication = (a, b) => a * b
 const division = (a, b) => a / b
-const erase = () => screen.innerText = ""
+const erase = () => {
+  screenText = []
+  writeToScreen(screenText)
+}
 
 
 keys.forEach((key) => {
@@ -21,14 +23,49 @@ keys.forEach((key) => {
     e.preventDefault()
     let keyPress = e.target.innerText
     screenText.push(keyPress)
+
+    if (keyPress === "=") {
+      const secondOperand = parseFloat(screenText.join(""))
+
+      let result = 0
+
+      switch (currentOperator) {
+        case "+":
+          result = addition(firstOperand, secondOperand)
+          break
+        case "-":
+          result = subtraction(firstOperand, secondOperand)
+          break
+        case "*":
+          result = multiplication(firstOperand, secondOperand)
+          break
+        case "/":
+          result = division(firstOperand, secondOperand)
+          break
+        default:
+          result = "Error"
+      }
+      screenText = [result]
+      writeToScreen(screenText)
+    }
+
+    if (keyPress === "C") {
+      erase()
+      currentOperator = null
+      firstOperand = null
+    }
     writeToScreen(screenText)
   })
 })
 
+let currentOperator = null
+let firstOperand = null
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (e) => {
     e.preventDefault()
-    let keyOperator = e.target.innerText
+    currentOperator = e.target.innerText
+    firstOperand = parseFloat(screenText.join(""))
+    screenText = []
   })
 })
